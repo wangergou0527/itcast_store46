25565ffcb54df7e2eab1411016e49c5b26e59dc6
shazi <138199527@qq.com>
2018-07-09 15:28:10 +0800

    登录—功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,9 +6,9 @@
         <el-input v-model="formData.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formData.password"></el-input>
+        <el-input type="password" v-model="formData.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button class="login-btn" type="primary" @click="handleLogin">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -20,6 +20,36 @@ export default {
       formData: {
         username: '',
         password: ''
+      }
+    }
+  },
+  methods: {
+    // handleLogin() {
+    //   this.$http
+    //     .post('login', this.formData)
+    //     .then((res) => {
+    //       const data = res.data
+    //       const { meta: { status, msg } } = data
+    //       if (status === 200) {
+    //         const { data: { token } } = data
+    //         sessionStorage.setItem('token', token)
+    //         this.$message.success(msg)
+    //       } else {
+    //         this.$message.error(msg)
+    //       }
+    //     })
+    // }
+
+    async handleLogin() {
+      const res = await this.$http.post('login', this.formData)
+      const data = res.data
+      const { meta: { status, msg } } = data
+      if (status === 200) {
+        const { data: { token } } = data
+        sessionStorage.setItem('token', token)
+        this.$message.success(msg)
+      } else {
+        this.$message.error(msg)
       }
     }
   }
