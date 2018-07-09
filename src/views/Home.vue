ccd1d31bafa7d30157499a343b9c695124a784b3
shazi <138199527@qq.com>
2018-07-09 17:04:37 +0800

    home-头部

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <img src="/static/logo.png">
+        </el-col>
+        <el-col class="middle" :span="19">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a class="logout" href="#">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -21,6 +33,20 @@ export default {
 
 .header {
   background-color: #b3c0d1;
+  padding: 2px;
+}
+
+.middle {
+  padding-right: 150px;
+  color: #fff;
+  line-height: 60px;
+  text-align: center;
+}
+
+.logout {
+  text-decoration: none;
+  line-height: 60px;
+  color: orange;
 }
 
 .aside {
