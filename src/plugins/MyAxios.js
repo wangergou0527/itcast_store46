a2f583fed3d7ad07332df60b1dd7eb1b540e1150
shazi <138199527@qq.com>
2018-07-10 20:37:44 +0800

    配置拦截器

--- src/plugins/MyAxios.js
+++ src/plugins/MyAxios.js
@@ -6,6 +6,27 @@ MyAxios.install = function (Vue) {
   var instance = axios.create({
     baseURL: 'http://localhost:8888/api/private/v1/'
   })
+  // Add a request interceptor
+  instance.interceptors.request.use(function (config) {
+  // Do something before request is sent
+    if (config.url.toLowerCase() !== 'login') {
+      var token = sessionStorage.getItem('token')
+      config.headers.Authorization = token
+    }
+    return config
+  }, function (error) {
+  // Do something with request error
+    return Promise.reject(error)
+  })
+
+  // Add a response interceptor
+  instance.interceptors.response.use(function (response) {
+  // Do something with response data
+    return response
+  }, function (error) {
+  // Do something with response error
+    return Promise.reject(error)
+  })
   Vue.prototype.$http = instance
 }
 
