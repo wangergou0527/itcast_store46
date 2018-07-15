ea2ef9ce90c0ed4361fc04d4ce7dd71d44a18eee
shazi <138199527@qq.com>
2018-07-15 21:42:35 +0800

    权限列表

---
+++ src/views/roies/Rights.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-card class="box-card">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-table
+      class="tb"
+      border
+      stripe
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="authName"
+        label="权限名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="path"
+        label="路径"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="level"
+        label="层级">
+        <template slot-scope="scope">
+          <span v-if="scope.row.level === '0'">一级</span>
+          <span v-else-if="scope.row.level === '1'">二级</span>
+          <span v-else-if="scope.row.level === '2'">三级</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.loadData()
+  },
+  methods: {
+    async loadData() {
+      const res = await this.$http.get('rights/list')
+      const data = res.data
+      this.list = data.data
+    }
+  }
+}
+</script>
+
+<style>
+.box-card {
+  height: 100%;
+  overflow: auto;
+}
+.tb {
+  margin-top: 10px;
+}
+</style>
